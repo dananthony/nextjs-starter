777463accfe49f5037ad5b5aa0fc45d4ebe010ea
Dan Anthony <dan@acolorbright.com>
2023-03-28 18:03:40 +0200

    chore: set up testing

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import Home from '../src/app/page';
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(<Home />);
+
+    const heading = screen.getByRole('heading', {
+      name: /Hello world! 👋/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
